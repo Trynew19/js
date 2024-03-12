948f03e4a21d9a66eeaa2fd347f9eb67ac088382
Trynew19 <129582242+Trynew19@users.noreply.github.com>
2024-03-12 09:43:33 +0000

    math and num

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,12 @@
+const name = String("krishna chauhan");
+
+
+// console.log(name.trim());
+
+// console.log(name.Slice(-1, 5));
+
+// slice 
+// replace
+// trim 
+// inclueds
+// splite array m convert karna
---
+++ 01_basic/06_numbers_maths.js
@@ -0,0 +1,54 @@
+const data = 500;
+
+
+const data2 = new Number(500);
+
+// console.log(data.toString().length);
+
+
+
+// console.log(data2.toFixed(2));
+
+
+const othersNumber = 123.2566
+// console.log(othersNumber.toPrecision(4));
+
+
+const hundred = 1000000000000;
+
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++ maths    +++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));  //change only - valur in positive value 
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(3.2));
+// console.log(Math);
+// console.log(Math.floor(5.2));
+// console.log(Math.min(1,2,3,4,5,6));
+// console.log(Math.max(5,555,2,63,5877));
+
+
+
+
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+
+// console.log(Math.floor(Math.random()*10) +1 );
+
+
+
+const min = 10 ;
+
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
