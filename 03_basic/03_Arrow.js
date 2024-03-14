c09da04149563cc68c480b208b5f91ddc4032a32
Trynew19 <129582242+Trynew19@users.noreply.github.com>
2024-03-14 06:23:29 +0000

    this and arrow fun com

---
+++ 03_basic/03_Arrow.js
@@ -0,0 +1,54 @@
+
+
+const user = {
+    username:"krishna chauhan",
+    age:20,
+    welcomeMessage : function () {
+        console.log(`${this.username}` ,"welcome to website");
+        // console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+
+// user.username = "rahul kuamr singh"
+
+// user.welcomeMessage()
+
+// console.log(this);
+
+
+// function krishna (){
+//     const username = "krishna chauhan"
+
+//     console.log(this);
+// }
+
+// krishna()
+
+// const krishna = function(){
+//     const username = "krishna chauhan"
+//     console.log(this);
+// }
+
+// krishna()
+
+const krishna = ()=>{
+    const username = "krishna chauhan"
+
+    console.log(this);
+}
+
+// krishna()
+
+
+// const addtwo = (num1,num2)=>{
+//     return num1 + num2
+// }
+
+// console.log(addtwo(3,5))
+
+// const addtwo = (num1,num2)=> num1 + num2            // or (num1 + num2 ) bhi likh sakte hai
+const addtwo = (num1,num2)=> (num1 + num2)          // it called is implshut return
+
+console.log(addtwo(55,666))
